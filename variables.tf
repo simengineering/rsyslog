c921e000018b6d63edbcad4064ce8fa0493be76f
Matthew Hopper <76822419+simengineering@users.noreply.github.com>
2021-10-09 09:28:47 -0400

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,38 @@
+variable "tenancy_ocid" {
+
+}
+
+variable "target_compartment_id" {
+
+}
+
+variable "vcn_id" {
+
+}
+
+variable "subnet_id" {
+
+}
+
+variable "instance_shape" {
+
+}
+
+#variable "generate_ssh_key_pair" {
+#  description = "Auto-generate SSH key pair"
+#  type        = string
+#}
+
+variable "ssh_public_key" {
+  
+}
+
+#variable "use_tenancy_level_policy" {
+#  description = "Compute instance to access all resources at tenancy level"
+#  type        = bool
+#}
+
+#variable "common_tags" {
+#  description = "Tags"
+#  type        = map(string)
+#}
