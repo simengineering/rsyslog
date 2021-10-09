0acc511e238ad7eea53c1a65823acc6f2f8b355b
Matthew Hopper <76822419+simengineering@users.noreply.github.com>
2021-10-09 09:35:30 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,30 +1,37 @@
 variable "tenancy_ocid" {
-
 }
 
 variable "target_compartment_id" {
-
 }
 
 variable "vcn_id" {
-
 }
 
 variable "subnet_id" {
-
 }
 
 variable "instance_shape" {
-
 }
 
-#variable "generate_ssh_key_pair" {
-#  description = "Auto-generate SSH key pair"
-#  type        = string
-#}
-
 variable "ssh_public_key" {
-  
+}
+
+variable "user_ocid" {
+}
+
+variable "fingerprint" {
+}
+
+variable "private_key_path" {
+}
+
+variable "region" {
+}
+
+variable "compartment_ocid" {
+}
+
+variable "ssh_private_key" {
 }
 
 #variable "use_tenancy_level_policy" {
@@ -35,4 +42,9 @@ variable "ssh_public_key" {
 #variable "common_tags" {
 #  description = "Tags"
 #  type        = map(string)
+#}
+
+#variable "generate_ssh_key_pair" {
+#  description = "Auto-generate SSH key pair"
+#  type        = string
 #}
