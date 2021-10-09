f496d92a1441e593c95e16dc3ee2245a5d0b4396
Matthew Hopper <76822419+simengineering@users.noreply.github.com>
2021-10-09 09:32:42 -0400

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,29 +1,4 @@
-// Copyright (c) 2017, 2021, Oracle and/or its affiliates. All rights reserved.
-// Licensed under the Mozilla Public License v2.0
 
-variable "tenancy_ocid" {
-}
-
-variable "user_ocid" {
-}
-
-variable "fingerprint" {
-}
-
-variable "private_key_path" {
-}
-
-variable "region" {
-}
-
-variable "compartment_ocid" {
-}
-
-variable "ssh_public_key" {
-}
-
-variable "ssh_private_key" {
-}
 
 provider "oci" {
   tenancy_ocid     = var.tenancy_ocid
