9133c1a2d84aa38dc036a9580293a84ba6869128
Matthew Hopper <76822419+simengineering@users.noreply.github.com>
2021-10-11 10:28:08 -0400

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -199,69 +199,12 @@ data "oci_core_instance_devices" "test_instance_devices" {
 
 # Output the private and public IPs of the instance
 
-output "instance_private_ips" {
-  value = [oci_core_instance.test_instance.*.private_ip]
-}
 
 output "instance_public_ips" {
   value = [oci_core_instance.test_instance.*.public_ip]
 }
 
-# Output the boot volume IDs of the instance
-output "boot_volume_ids" {
-  value = [oci_core_instance.test_instance.*.boot_volume_id]
-}
-
-# Output all the devices for all instances
-output "instance_devices" {
-  value = [data.oci_core_instance_devices.test_instance_devices.*.devices]
-}
 
 
-/*
-output "attachment_instance_id" {
-  value = data.oci_core_boot_volume_attachments.test_boot_volume_attachments.*.instance_id
-}
-*/
 
-resource "oci_core_vcn" "terraformVCN" {
-  cidr_block     = "10.0.0.0/16"
-  compartment_id = var.compartment_ocid
-  display_name   = "terraformVCN"
-  dns_label      = "terraformVCN"
-}
-
-resource "oci_core_internet_gateway" "test_internet_gateway" {
-  compartment_id = var.compartment_ocid
-  display_name   = "TestInternetGateway"
-  vcn_id         = oci_core_vcn.terraformVCN.id
-}
-
-resource "oci_core_default_route_table" "default_route_table" {
-  manage_default_resource_id = oci_core_vcn.terraformVCN.default_route_table_id
-  display_name               = "DefaultRouteTable"
-
-  route_rules {
-    destination       = "0.0.0.0/0"
-    destination_type  = "CIDR_BLOCK"
-    network_entity_id = oci_core_internet_gateway.test_internet_gateway.id
-  }
-}
-
-resource "oci_core_subnet" "terraformSUBNET" {
-  availability_domain = data.oci_identity_availability_domain.ad.name
-  cidr_block          = "10.0.0.0/24"
-  display_name        = "terraformSUBNET"
-  dns_label           = "terraformSUBNET"
-  security_list_ids   = [oci_core_vcn.terraformVCN.default_security_list_id]
-  compartment_id      = var.compartment_ocid
-  vcn_id              = oci_core_vcn.terraformVCN.id
-  route_table_id      = oci_core_vcn.terraformVCN.default_route_table_id
-  dhcp_options_id     = oci_core_vcn.terraformVCN.default_dhcp_options_id
-}
-
-data "oci_identity_availability_domain" "ad" {
-  compartment_id = var.tenancy_ocid
-  ad_number      = 1
-}
 
